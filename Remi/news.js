34d109ec991ba7bf10c6daccbf443b779cd3240c
remi ibraheem <remicodesmuintu@gmail.com>
2020-05-08 22:04:58 -0400

    added articles descriptions to cardsas well as selector id forimages and all buttons onto the html. Wrote simple pseudo code.

--- Remi/news.js
+++ Remi/news.js
@@ -4,11 +4,81 @@ $.ajax({
   url: queryURL,
   method: 'GET',
 }).then(function (response) {
-  console.log(response);
-  console.log(response.articles[0].author);
+console.log(response);
+// console.log(response.articles[0].author);
 
-  // var content = response.wind.speed;
-  // $("#windSpeed").append(content);
-});
-// ---------------------------------------------------------
+//Add images to the cards
 
+
+
+//add description to cards/ Next, I need to figure out how to itterate this and append dynamically
+var cardContent =response.articles[0].content;
+$("#description-1").append(cardContent);
+
+var cardContent = response.articles[1].content;
+$("#description-2").append(cardContent);
+
+var cardContent = response.articles[2].content;
+$("#description-3").append(cardContent);
+
+var cardContent = response.articles[3].content;
+$("#description-4").append(cardContent);
+
+var cardContent = response.articles[4].content;
+$("#description-5").append(cardContent);
+
+var cardContent = response.articles[6].content;
+$("#description-6").append(cardContent);
+
+var cardContent = response.articles[7].content;
+$("#description-7").append(cardContent);
+
+var cardContent = response.articles[8].content;
+$("#description-8").append(cardContent);
+
+var cardContent = response.articles[9].content;
+$("#description-9").append(cardContent);
+
+var cardContent = response.articles[10].content;
+$("#description-10").append(cardContent);
+
+var cardContent = response.articles[13].content;
+$("#description-11").append(cardContent);
+
+var cardContent = response.articles[14].content;
+$("#description-12").append(cardContent);
+
+
+//add events listeners to article object
+var buttonEl = response.articles[0].url;
+console.log(response.articles[0].url);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            });
+
+// $.ajax({
+// url: queryURL,
+// method: 'GET',
+// }).then(function (response) {
+// console.log(response);
+// console.log(response.Runtime);
+// var content = response.wind.speed;v
+// $("#windSpeed").append(content);
+// });
--- Remi/remi-pseudocodes.md
+++ Remi/remi-pseudocodes.md
@@ -2,8 +2,12 @@
 
 #   connect cards to news links
 
-
 #   link articles to cards
 1.  create classes for individual cards
 2.  acquire api websites url and key
 3.  create ajax function for each card
+//Add images to the cards
+
+//add description to cards/ Next, I need to figure out how to itterate this and append dynamically
+
+//add events listeners to article object
